e1ed8baebde719d4f086ddab12a34e6ce465e4cb
Mohau <mohaumodisana@gmail.com>
2021-09-16 00:12:27 +0200

    now able to get values from the inputs and radio buttons

--- index.js
+++ index.js
@@ -1,30 +1,52 @@
-const { response } = require('express');
 const express = require('express');
 const exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+const SettingsBill = require('./settings-bill');
+
 let app = express();
+
+const settingsBill = SettingsBill();
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.get('/', function(req, res){
-    res.render('index');
+    // res.render('index');
+    res.render('index', {
+        // settings: settingsBill.getSettings(),
+         totals: settingsBill.totals()
+    });
 });
 
-app.post('/settings ', function(req, res){
+app.post('/settings', function(req, res){
+
+    settingsBill.setSettings({
+        callCost: req.body.callCost,
+        smsCost: req.body.smsCost,
+        warningLevel: req.body.warningLevel,
+        criticalLevel: req.body.criticalLevel
+    });
+    console.log(settingsBill.getSettings());
+   res.redirect('/');
+});
+
+app.post('/action', function(req, res){
+
+    console.log(req.body.billItemTypeWithSettings);
+    settingsBill.recordAction(req.body.actionType)
+    res.redirect('/');
+});
+
+app.get('/actions', function(req, res){
 
 });
 
-app.post('/action ', function(req, res){
-
-});
-
-app.get('/actions ', function(req, res){
-
-});
-
-app.post('/actions/:type ', function(req, res){
+app.post('/actions/:type', function(req, res){
 
 });
 
