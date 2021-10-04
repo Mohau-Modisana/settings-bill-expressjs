a21464b0486307bf0516638bede23b4a250dc36e
Mohau <mohaumodisana@gmail.com>
2021-10-04 13:24:05 +0200

    Debugging and added other files

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const exphbs  = require('express-handlebars');
 const bodyParser = require('body-parser');
 const SettingsBill = require('./settings-bill');
+
 const handlebarSetup = exphbs({
     partialsDir: "./views/partials",
     viewPath:  './views',
@@ -43,16 +44,17 @@ app.post('/settings', function(req, res){
 app.post('/action', function(req, res){
 
     console.log(req.body.billItemTypeWithSettings);
-    settingsBill.recordAction(req.body.actionType)
+    settingsBill.recordAction(req.body.billItemTypeWithSettings)
     res.redirect('/');
 });
 
 app.get('/actions', function(req, res){
-
+    res.render('actions', {actions: settingsBill.actions()});
 });
 
-app.post('/actions/:type', function(req, res){
-
+app.post('/actions/:actionType', function(req, res){
+    const actionType = req.params.actionType; 
+    res.render('actions', {actions: settingsBill.actionsFor(actionType)});
 });
 
 const PORT = process.env.PORT || 3011;
